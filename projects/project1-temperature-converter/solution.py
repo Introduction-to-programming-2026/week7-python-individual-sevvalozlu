89c2306d7698b7bf6e492f9decc92df01c617f57
sevvalozlu <ozlu.sevval@ibu.edu.mk>
2026-04-08 19:35:11 +0200

    solve project 1

--- projects/project1-temperature-converter/solution.py
+++ projects/project1-temperature-converter/solution.py
@@ -1,6 +1,6 @@
 # Project 1 — Temperature Converter
-# Author: your name here
-# Date:   session date here
+# Author: Şevval Özlü
+# Date:   8 April 2026
 #
 # Instructions:
 #   1. Read the README.md in this folder first.
@@ -12,10 +12,23 @@
 celsius = float(input("Enter temperature in Celsius: "))
 
 # TODO: calculate fahrenheit using the formula F = (C × 9/5) + 32
-# fahrenheit = ...
+ fahrenheit = (celsius * 9/5) + 32
 
 # TODO: print the result using an f-string
-# print(f"...")
+ print(f"{celsius}°C = {fahrenheit}°F")
+# 
+# 
 
 # ── Bonus (optional) ─────────────────────────────────────────────────────────
+choice = input("Convert: (1) Celsius to Fahrenheit  (2) Fahrenheit to Celsius: ")
+
+if choice == "1":
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (celsius * 9/5) + 32
+    print(f"{celsius}°C = {fahrenheit}°F")
+
+elif choice == "2":
+    fahrenheit = float(input("Enter temperature in Fahrenheit: "))
+    celsius = (fahrenheit - 32) * 5/9
+    print(f"{fahrenheit}°F = {celsius}°C")
 # Add a direction menu (C→F or F→C)
