4ad2b5a5fd458610a0d17893eba1c6afe2fc15f2
sevvalozlu <ozlu.sevval@ibu.edu.mk>
2026-04-08 20:48:02 +0200

    Sevval:  solve Project 4 - word counter

--- projects/project4-word-counter/solution.py
+++ projects/project4-word-counter/solution.py
@@ -1,17 +1,33 @@
 # Project 4 — Word Counter
-# Author: your name here
+# Author: Şevval Özlü
 
 sentence = input("Enter a sentence: ")
+#BONUS: remove punctuation
+sentence = sentence.replace(",", "").replace("!", "").replace("?", "")
+
 words = sentence.lower().split()
 
 # TODO: total word count using len()
+total_words =len(words)
 
 # TODO: character count (no spaces)
+total_characters = len(sentence.replace(" ", ""))
 # Hint: sentence.replace(" ", "") removes all spaces, then use len()
 
 # TODO: word frequency dictionary
-# frequency = {}
-# for word in words:
-#     ...
+frequency = {}
+for word in words:
+    if word in frequency:
+      frequency[word] += 1
+    else:
+      frequency[word] = 1
+#BONUS: sort from most to least common
+sorted_words = sorted(frequency.items(), key=lambda x: x[1], reverse=True)
 
 # TODO: print total words, total characters, then word frequency
+print(f"Total words: {total_words}")
+print(f"Total characters (no spaces): {total_characters}")
+print("Word frequency:")
+
+for word, count in sorted_words:
+  print(f"{word} -> {count}")
